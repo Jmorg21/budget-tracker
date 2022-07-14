e2f17d621a4e66cb4ec393724f5986aa7f045eed
Jmorg21 <Jmorg21@gmail.com>
2022-07-13 23:14:57 -0400

    tweaking for heroku

--- server.js
+++ server.js
@@ -3,12 +3,8 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/pizza-hunt', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
-
-mongoose.set('debug', true);
+const PORT = process.env.PORT || 3001;
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget-tracker";
 
 const app = express();
 
@@ -20,4 +16,13 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-app.use(require("./Develop/routes/api.js"));
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+app.use(require("./routes/api.js"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
